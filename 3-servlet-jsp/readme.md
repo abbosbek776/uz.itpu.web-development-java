fd2c57087b3067a98ea7fe433b0016d5ceb6f11b
Andrei <rohau.andrei@gmail.com>
2024-02-04 23:17:52 +0300

    simple servlet

---
+++ 3-servlet-jsp/readme.md
@@ -0,0 +1,5 @@
+1. maven install
+2. copy [uz.itpu.web-development-java\3-servlet-jsp\target\3-servlet-jsp-1.0-SNAPSHOT.war]() to [..\apache-tomcat-9.0.85\webapps]()
+3. run [..\apache-tomcat-9.0.85\bin\startup.bat]()
+4. call [http://localhost:8080/3-servlet-jsp-1.0-SNAPSHOT](http://localhost:8080/3-servlet-jsp-1.0-SNAPSHOT)
+5. call [http://localhost:8080/3-servlet-jsp-1.0-SNAPSHOT/FrontController](http://localhost:8080/3-servlet-jsp-1.0-SNAPSHOT/FrontController)
---
+++ 3-servlet-jsp/src/main/java/org/itpu/FrontController.java
@@ -0,0 +1,16 @@
+package org.itpu;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FrontController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("FrontController.doGet()");
+        req.getRequestDispatcher("home.jsp").forward(req, resp);
+    }
+}
