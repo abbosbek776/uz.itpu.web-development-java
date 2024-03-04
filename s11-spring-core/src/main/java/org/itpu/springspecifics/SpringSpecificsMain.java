d28701ae272b3bdecf46057471ade6dd563e17c6
arohau <arohau@edmunds.com>
2024-03-04 04:05:25 +0300

    11-spring-core

--- s11-spring-core/src/main/java/org/itpu/springspecifics/SpringSpecificsMain.java
+++ s11-spring-core/src/main/java/org/itpu/springspecifics/SpringSpecificsMain.java
@@ -1,13 +1,12 @@
 package org.itpu.springspecifics;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Component;
 
+//@Configuration
+@Component
 @ComponentScan("org.itpu.springspecifics")
 @PropertySource("classpath:/application.properties")
-@Configuration
 public class SpringSpecificsMain {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringSpecificsMain.class);
@@ -16,8 +15,43 @@ public class SpringSpecificsMain {
         someBean.doSomething();
         someBean = null;
 
+        Object customBean1a = context.getBean("customBean");
+        Object customBean1b = context.getBean("customBean");
+        Object customBean2a = context.getBean("customBeanTwo");
+        Object customBean2b = context.getBean("customBeanTwo");
+
         // this will release resources
         context.registerShutdownHook();
 
     }
+
+    @Bean
+    public Object customBean() {
+        return new Object();
+    }
+
+    @Bean
+    public Object customBeanTwo() {
+        return customBean();
+    }
+
+    /*
+    @Configuration vs @Component
+
+    you do can create i.e SpringSpecificsMain.java class then stereotype with @Component and add @Beans declaration to it.
+    In this way it will look as a configuration, main difference is that when annotated class with @Configuration @Bean
+    annotated methods are proxy using CGLIB which made in code calls after the first one to return bean from context
+    instead of execute method again and create another instance as happens when using @Component with @Bean
+
+    Example
+    @Configuration
+    customBean1a == customBean1b == customBean2a == customBean2b
+
+    @Component
+    customBean1a == customBean1b
+    customBean2a == customBean2b
+    BUT
+    customBean1a != customBean2a
+
+     */
 }
