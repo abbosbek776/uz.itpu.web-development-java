daed9f7e4f5c2479e2bdded96642bb8db442643d
arohau <arohau@edmunds.com>
2024-03-14 00:01:49 +0300

    12-spring-core - home task

--- s12-spring-core/src/main/java/org/itpu/home/task/AppConfig.java
+++ s12-spring-core/src/main/java/org/itpu/home/task/AppConfig.java
@@ -5,25 +5,25 @@ import org.springframework.context.annotation.Bean;
 
 public class AppConfig {
     @Bean
-    public Task task(@Qualifier("assignee") Employee e1,
-                     @Qualifier("reviewer") Employee e2) {
+    public Task task(@Qualifier("assignee") Employee arg1,
+                     @Qualifier("reviewer") Employee arg2) {
         Task task = new Task();
-        task.setAssignee(e1);
-        task.setReviewer(e2);
+        task.setAssignee(arg1);
+        task.setReviewer(arg2);
         task.setDescription("New feature");
         return task;
     }
 
-    @Bean(name = "assignee")
-    public Employee employee1() {
+    @Bean
+    public Employee assignee() {
         Employee assignee = new Employee();
         assignee.setName("John Doe");
         assignee.setPosition("Junior Software Engineer");
         return assignee;
     }
 
-    @Bean(name = "reviewer")
-    public Employee employee2() {
+    @Bean
+    public Employee reviewer() {
         Employee reviewer = new Employee();
         reviewer.setName("Emily Brown");
         reviewer.setPosition("Senior Software Engineer");
