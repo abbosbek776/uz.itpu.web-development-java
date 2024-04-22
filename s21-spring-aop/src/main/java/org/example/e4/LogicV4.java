e08f0d45b120bf4de54cbe6334a0ca6fc373aa42
arohau <arohau@edmunds.com>
2024-04-22 05:26:23 +0300

    aspect Pointcut execution args

---
+++ s21-spring-aop/src/main/java/org/example/e4/LogicV4.java
@@ -0,0 +1,16 @@
+package org.example.e4;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogicV4 {
+
+    public int textLength(String text) {
+        System.out.println("Logic#textLength()");
+        return text.length();
+    }
+
+    public void perform() {
+        System.out.println("Logic#perform()");
+    }
+}
