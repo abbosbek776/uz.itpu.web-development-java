1fbbb3ad37116245291db0909126951a4cfa1602
arohau <arohau@edmunds.com>
2024-04-22 05:41:32 +0300

    aspect Pointcut JoinPoint info

---
+++ s21-spring-aop/src/main/java/org/example/e6/Application.java
@@ -0,0 +1,17 @@
+package org.example.e6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        LogicV6 logic = context.getBean(LogicV6.class);
+
+        logic.combineValues("asd", 123);
+    }
+}
---
+++ s21-spring-aop/src/main/java/org/example/e6/LogicV6.java
@@ -0,0 +1,11 @@
+package org.example.e6;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogicV6 {
+
+    public String combineValues(String a, int b) {
+        return a + b;
+    }
+}
